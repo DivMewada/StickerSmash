08e8303186927b94cb9efd92efd4d7edf718710d
Divya <divyasuthar1605@gmail.com>
2025-01-29 17:57:07 +0530

    Add link to about screen on index

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router"
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Link href= "/about" style={styles.button}> Go to about screen </Link>
     </View>
   );
 }
@@ -16,6 +18,11 @@ const styles = StyleSheet.create ({
     alignItems: "center"
   },
   text: {
+    color: "#fff"
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
     color: "#fff"
   }
 })
