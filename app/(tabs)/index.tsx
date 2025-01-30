2e881dbe2c1f8e1146332215f00a8355a1da93d9
Divya <divyasuthar1605@gmail.com>
2025-01-30 14:36:08 +0530

    Add image picker library. Open image picker on click of a button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import * as ImagePicker from "expo-image-picker";
+
 const PlaceHolderImage = require("@/assets/images/background-image.png")
 
 export default function Index() {
+  const pickerImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      quality: 1,
+      mediaTypes: ['images'],
+      allowsEditing: true
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image");
+      return;
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceHolderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" />
+        <Button label="Choose a photo" theme="primary" onPress={pickerImageAsync} />
         <Button label="Use this photo" />
       </View>
     </View>
