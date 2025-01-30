a64bd3ef6c775653f124a11770e6ab3f262f17cb
Divya <divyasuthar1605@gmail.com>
2025-01-30 16:03:57 +0530

    Add emojipicker component. Use it to show modal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,12 +5,14 @@ import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
 
 const PlaceHolderImage = require("@/assets/images/background-image.png")
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const pickerImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -33,7 +35,11 @@ export default function Index() {
   }
 
   const onAddSticker = () => {
+    setIsModalVisible(true);
+  }
 
+  const onModalClose = () => {
+    setIsModalVisible(false);
   }
 
   const onSaveImageAsync = () => {
@@ -59,7 +65,9 @@ export default function Index() {
           <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
         </View>
       )};
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
       
+      </EmojiPicker>
     </View>
   );
 }
